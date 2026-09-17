c918e7ab10d2464c5fe558a1d501c8b07f7d9713
IMT <imesonero@outlook.com>
2026-09-17 17:46:54 +0000

    hola

--- Inseccion.py
+++ Inseccion.py
@@ -8,3 +8,29 @@ def inseccion(lista):
 lista1 = [7,4,5,6,43,3,2,77,8,0,1,12]
 list2 = inseccion(lista1)
 print(lista2)       
+
+#for i = N-1 to 1:
+    #if A[i] < A[i-1], then swap A[i] and A[i-1]
+    #else stop
+
+def inseccion1(lista):  #NO FUNCIONA
+    for i in range(1, len(lista)):
+        j = i - 1
+        while j >= 0 and lista[i] < lista[j]:
+            lista[i], lista[j] = lista[j], lista[i]
+            j -= 1
+    return lista
+
+lista1 = [7,4,5,6,43,3,2,77,8,0,1,12]
+lista3 = inseccion1(lista1)
+print(lista3)
+
+def insertion_sort(data):    #EJEMPLO DE FAUSTO
+    for i in range(1, len(data)):
+        key = data[i]
+        j = i - 1
+        while j >= 0 and key < data[j]:
+            data[j + 1] = data[j]
+            j -= 1
+        data[j + 1] = key
+    return data
